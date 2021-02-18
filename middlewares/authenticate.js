1f75b4a91b4e2e9fe3282c25cbdb12f4d3171ff4
ardyanrizki <surel.ardy@gmail.com>
2021-02-19 01:48:16 +0700

    fix authentication, test client to server

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,8 +1,21 @@
+const {Player} = require('../models')
+
 const autenticate = (req,res,next)=>{
     try {
-        const {name} = req.headers
-        req.decoded = {name}
-        next()
+        const name = req.headers.name
+
+        Player.findOne({
+            where: {
+                name
+            }
+        })
+            .then(player => {
+                req.decoded = {name: player.name}
+                next()
+            })
+            .catch(err => {
+                console.log(err);
+            })
     } catch (error) {
         throw({name:"InvalidToken", msg:"Invalid Token"})
     }
