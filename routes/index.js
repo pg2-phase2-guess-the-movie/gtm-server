00ea549606749d4f31b443f2e74eda06b135c4e8
Rizky Rahman <rizkyrahmn@gmail.com>
2021-02-19 13:59:24 +0700

    Update socket.io 3

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router()
-const authenticate = require("../middlewares/authenticate")
-const PlayerController = require("../controllers/PlayerController")
-
-router.post("/login", PlayerController.newPlayer)
-router.get("/quiz", PlayerController.getQuiz)
-
-module.exports = router
